0946c5611789054322b69085abeebdc479596e71
allegra <allegra.seeler@ens-lyon.fr>
2021-11-17 14:00:36 +0000

    Added info on data source

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # TP NGS
+
 This R Markdown document will contain all the information necessary to understand the code we will create. 
+
+## Origin of the data
+scRNAseq data of A. thaliana root protoplasts. Protoplasts are cells from which the cell wall was removed. 
+The scRNAseq was obtained with a 10X Genomics Chromium protocol
+
+The data is in the GEO database (NCBI) under the accession code GSE123013.
+We will study samples from one wild-type and one mutant
 
---
+++ script/download_data.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#On définit le directory pour que toutes les données soient bien stockées dans le dossier 'data'
+cd ~/mydatalocal/tp_ngs_scarabi
+
+#On créé une variable avec tous les SRR qu'on veut télécharger 
+srr=("SRR8257101 SRR8257104")
+
+#On lançe une boucle for pour télécharger toutes les datas dont le SRR est dans la variable SRR
+for sample in $srr
+  do 
+#On indique quels SRR prendre (en mettant bien un $ pour rentrer dans la variable SRR). 
+#Puis on dit de paralléliser 8 threads en parallèle pour aller plus vite. 
+#Puis on donne le output directory 
+#Puis on dit de split les files pour télécharger plus vite
+#Puis ont gzip et je sais pas à quoi ça sert 
+
+  parallel-fastq-dump --sra-id $sample\
+                      --threads 8\
+                      --outdir data/\
+                      --split-files\
+                      --gzip
+  done
+    
+  
