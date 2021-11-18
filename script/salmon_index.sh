3c6b11d5f5a5cd8ced4944ca8f0c725755aa11ca
allegra <allegra.seeler@ens-lyon.fr>
2021-11-18 10:51:44 +0000

    Downloading salmon index

---
+++ script/salmon_index.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+cd ~mydatalocal/tp_ngs_scarabi/
+
+salmon index -t data/Transcriptome/transcriptome.fa -k 31 -i data/Transcriptome/salmon \
+--features data/Transcriptome/transcriptome.gtf -p 8
