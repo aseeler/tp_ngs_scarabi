159b365a068bf89be327985a6c602e1ff56f9865
allegra <allegra.seeler@ens-lyon.fr>
2021-11-18 10:34:15 +0000

    Script to retrieve the transcriptome data from online

---
+++ script/retrieve_transcriptome.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd ~/mydatalocal/tp_ngs_scarabi 
+#To retrieve the transcriptome of A. thaliana for the Alevin analysis
+
+wget https://ics.hutton.ac.uk/atRTD/RTD2/AtRTD2_19April2016.fa \
+ -O data/Transcriptome/transcriptome.fa
