bed85c1a675f6872fd8ef50225d210da06b5bc1d
allegra <allegra.seeler@ens-lyon.fr>
2021-11-18 13:28:26 +0000

    Modified script to create salmon index properly

--- script/salmon_index.sh
+++ script/salmon_index.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-cd ~mydatalocal/tp_ngs_scarabi/
+cd ~/mydatalocal/tp_ngs_scarabi/
 
-salmon index -t data/Transcriptome/transcriptome.fa -k 31 -i data/Transcriptome/salmon \
---features data/Transcriptome/transcriptome.gtf -p 8
+salmon index -t data/Transcriptome/transcriptome.fa -k 31 -i data/Transcriptome/salmon -p 8
