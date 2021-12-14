61a67c73f67e8a9e8aceac0763b58415e90ef0be
allegra <allegra.seeler@ens-lyon.fr>
2021-12-14 14:23:47 +0000

    Retrieving csv files with markers for cluster analysis

---
+++ script/retrieve_markers.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+cd ~/mydatalocal/tp_ngs_scarabi/
+
+wget --user=tp_ngs --password='Arabido2021!' https://flower.ens-lyon.fr/tp_ngs/scarabi/Li2016/Markers.csv
+
+#pas besoin de -r car ce n'est pas récursif
